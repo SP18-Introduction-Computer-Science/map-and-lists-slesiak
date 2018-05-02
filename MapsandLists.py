7cc94fab76ffcddae5fec9f7dcdba7de439f5e8c
slesiak <35575507+slesiak@users.noreply.github.com>
2018-05-01 23:30:37 -0500

    MapsandLists.py
    Sheila's Maps and Lists Assignment

---
+++ MapsandLists.py
@@ -0,0 +1,7 @@
+a = [0, 2, 4, 6, 8, 10]
+for element in a :
+    print(element)
+    
+map = {1:'red', 2:'orange', 3:'yellow', 4:'green', 5:'blue', 6:'violet'}
+for key in map :
+    print(str(key) + ":" + str(map[key]))
